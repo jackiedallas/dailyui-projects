0ecf3834e45f0e647bb6399ddf223eac871c2f35
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-15 01:18:14 -0500

    added navbar component

---
+++ react-website-challenge-1/src/components/navbar.js
@@ -0,0 +1,16 @@
+class navbar extends React.Component {
+	render() {
+		return (
+			<div className="navbar">
+				<ul>
+					<li>Instagram</li>
+					<li>Facebook</li>
+					<li>Twitter</li>
+					<li>LinkedIn</li>
+				</ul>
+			</div>
+		)
+		
+	}
+	
+}
